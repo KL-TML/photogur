bdd45c5e01be2ead0a83b1f597cc60b6c267eb6a
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-20 20:18:30 -0500

    delete method

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -17,13 +17,29 @@ class PicturesController < ApplicationController
     @picture.artist = params[:picture][:artist]
     @picture.url = params[:picture][:url]
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end
+
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
 
     if @picture.save
-      # if the picture gets saved, generate a get request to "/pictures" (the index)
-      redirect_to "/pictures"
+      redirect_to "/pictures/#{@picture.id}"
     else
-      # otherwise render new.html.erb
-      render :new
+      render :edit
     end
   end
 

6e31db3df1ec67600a67eb9ab43a3c0be5462540
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-20 19:16:16 -0500

    create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new'
+
   get 'pictures/:id' => 'pictures#show'
+  
 end
